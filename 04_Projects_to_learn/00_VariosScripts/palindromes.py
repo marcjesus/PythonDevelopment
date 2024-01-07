4bb2739e44151375a2f96830e1677b05b89761ce
marcjesus <marcjesusgarcia@gmail.com>
2024-01-07 15:40:36 +0100

    commit

---
+++ 04_Projects_to_learn/00_VariosScripts/palindromes.py
@@ -0,0 +1,20 @@
+
+word = ['racecar', 'shoe', 'moon']
+
+def reverse_word (word):
+    reversed = ""
+    for letter in word:
+            reversed = letter + reversed
+
+    return reversed
+
+def check_all_palindromes( arr):
+    if arr[0] == reverse_word(arr[0])
+            if arr[1] == reverse_word(arr[1])
+                    if arr[2] == reverse_word(arr[2])
+                            return true
+    return false
+
+
+if __name__ == '__main__':
+       pass
