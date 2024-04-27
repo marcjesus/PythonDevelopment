fddcf43e5ea610a511ce744a2e46a54d38c337d0
Marc Jesus <marcjesusgarcia@gmail.com>
2024-04-27 09:01:27 +0100

    commit

--- playground.py
+++ playground.py
@@ -1,19 +1,9 @@
-def is_leap(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        return True
-    else:
-        return False
-"""
-    if year % 4 == 0:
-        leap = "True"
-    elif year % 100 == 0 and year % 400 == 0:
-        leap = "True"
-    elif year % 400 == 0:
-        leap = "True"
-    else:
-        leap = "False"
-"""
-
-
-year = int(input())
-print(is_leap(year))
+if __name__ == '__main__':
+    n = input("Enter a number")
+    result = ""
+    for i in range(1,int(n)+1):
+        result=str(i) + result
+        print(f"Result = {str(i)} + {result}")
+    
+    reorder = result[::-1]
+    print(reorder)
